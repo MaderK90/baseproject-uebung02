078c0e8ac34745ccf1ed83c4cea086131e51b395
Katharina Mader <katharinamader@katharinas-air.home>
2020-03-25 20:16:08 +0100

    Übung2 fertig

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'testdatabase');
-define('DB_USER', 'testuser');
-define('DB_PASS', 'testpass');
+define('DB_NAME', 'baseproject');
+define('DB_USER', 'baseproject');
+define('DB_PASS', 'ZfuG1reiIU1Mr8Ci');
